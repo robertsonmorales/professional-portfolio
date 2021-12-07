95a191af5ed18fe636f10bac768d9504e334ab68
Robertson Morales <moralesso152842@gmail.com>
2021-12-07 22:04:31 +0800

    fixed email validation

--- resources/js/contact.js
+++ resources/js/contact.js
@@ -5,10 +5,10 @@ $(() => {
     event.preventDefault();
 
     isEmpty($('#name').val(), $('#name').attr('id'), $('#name').attr('name'));
-    isEmpty($('#email_address').val(), $('#email_address').attr('id'), $('#email_address').attr('name'));
+    var email = isEmpty($('#email_address').val(), $('#email_address').attr('id'), $('#email_address').attr('name'));
+    email ? '' : validateEmail($('#email_address').val());
     isEmpty($('#subject').val(), $('#subject').attr('id'), $('#subject').attr('name'));
     isEmpty($('#message').val(), $('#message').attr('id'), $('#message').attr('name'));
-    validateEmail($('#email_address').val());
 
   });
 
@@ -23,15 +23,20 @@ $(() => {
       $('#'+id).addClass('is-invalid');
       $('#validate-'+id).addClass('d-block');
       $('#validate-'+id).html('The ' + newName + ' is a required field.');
+
+      return true;
     }else{
       $('#'+id).removeClass('is-invalid');
       $('#validate-'+id).removeClass('d-block');
+
+      return false;
     }
   }
 
   function validateEmail(email) {
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (!email.match(regexEmail)) {
+      $('#email_address').addClass('is-invalid');
       $('#validate-email_address').addClass('d-block');
       $('#validate-email_address').html('Invalid email address.');
 
